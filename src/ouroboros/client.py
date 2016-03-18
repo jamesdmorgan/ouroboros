d6951c3972cddedc0efaf4ac2952e18566185c66
Bob Gregory <bob.gregory@made.com>
2016-03-18 09:22:01 +0000

    First method for creating streams, also tidied up content-types

--- src/ouroboros/client.py
+++ src/ouroboros/client.py
@@ -3,6 +3,12 @@ import requests
 from requests.auth import HTTPBasicAuth
 import pprint
 from urllib.parse import urljoin
+import uuid
+
+
+JSON="application/json"
+ATOM="application/vnd.eventstore.atom+json"
+EVENTS="application/vnd.eventstore.events+json"
 
 User = namedtuple(
     'user', [
@@ -25,10 +31,10 @@ class UserManager:
                              "password": password,
                              "fullName": fullname or username,
                              "groups": groups
-                             })
+                             }, JSON)
 
     def get(self, username):
-        response = self.client.get_json('/users/'+username)
+        response = self.client.get('/users/'+username, JSON)
         if response.status_code == 404:
             raise UserNotFoundException()
         data = response.json()['data']
@@ -53,7 +59,7 @@ class UserManager:
         response = self.client.put(user.links['edit'], {
             "fullName": user.full_name,
             "groups": list(groups)
-        })
+        }, JSON)
 
     def removegroup(self, username, *args):
         user = self.get(username)
@@ -63,14 +69,27 @@ class UserManager:
         response = self.client.put(user.links['edit'], {
             "fullName": user.full_name,
             "groups": list(groups)
-        })
+        }, JSON)
 
     def rename(self, username, full_name):
         user = self.get(username)
         response = self.client.put(user.links['edit'], {
             "fullName": full_name,
             "groups": user.groups
-        })
+        }, JSON)
+
+
+class StreamManager:
+
+    def __init__(self, client):
+        self.client = client
+
+    def create(self, name, eventid=None):
+        metadata = [{
+            "eventId": str(eventid or uuid.uuid4()),
+            "eventType": "$user-updated"
+        }]
+        self.client.post("/streams/"+name+"/metadata", metadata, EVENTS)
 
 
 class Client:
@@ -79,13 +98,14 @@ class Client:
         scheme = "http" if no_ssl else "https"
         self.base_uri = "{0}://{1}:{2}".format(scheme, host, port)
         self.users = UserManager(self)
+        self.streams = StreamManager(self)
         self.username = username
         self.password = password
 
     def get_uri(self, path):
         return urljoin(self.base_uri, path)
 
-    def post(self, path, body):
+    def post(self, path, body, content_type):
         response = requests.post(
             self.get_uri(path),
             json=body,
@@ -93,25 +113,22 @@ class Client:
                 self.username,
                 self.password),
             headers={
-                'Content-Type': 'application/json'})
+                'Content-Type': content_type})
         print(response)
 
-    def get(self, path):
+    def get(self, path, content_type):
         return requests.get(self.get_uri(path)+'?embed=tryharder',
                             headers={
-                                'Accept': 'application/vnd.eventstore.atom+json'
+                                'Accept': content_type
                                 })
 
-    def get_json(self, path):
-        return requests.get(self.get_uri(path)+'?embed=tryharder',
-                            headers={
-                                'Content-Type': 'application/json'
-                                })
-
-    def put(self, path, body):
+    def put(self, path, body, content_type):
         return requests.put(self.get_uri(path),
                             auth=HTTPBasicAuth(self.username, self.password),
-                            json=body)
+                            json=body,
+                            headers={
+                                'Content-Type': content_type
+                            })
 
     def delete(self, path):
         return requests.delete(self.get_uri(path),
